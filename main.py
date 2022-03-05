c40e4978bbdd5df14ba49db5079dca4e85476649
eugenii <eugenii.cher@gmail.com>
2022-03-05 16:32:18 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_ngrok import run_with_ngrok
+
+app = Flask(__name__)
+run_with_ngrok(app)
+
+
+@app.route("/")
+def index():
+    return "Привет от приложения Flask with ngrok"
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
